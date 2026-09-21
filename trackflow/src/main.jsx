b1f3875997a8b7b444530fdb187b5852b4768198
KITAN_MARVEAUX <aloemmanuel369@gmail.com>
2026-09-21 15:40:59 +0100

    chore(skycast): remove legacy router and toast bootstrap

--- trackflow/src/main.jsx
+++ trackflow/src/main.jsx
@@ -1,16 +1,10 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import 'react-toastify/dist/ReactToastify.css'
-import { ToastContainer } from 'react-toastify'
 import App from './App'
 import './styles/index.css'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-      <ToastContainer />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 )
